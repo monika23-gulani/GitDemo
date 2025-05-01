d511cf657b66c77469ef8f1ef754621f29a1322d
monika23-gulani <monika.gulani1989@gmail.com>
2025-05-01 18:02:53 +0530

    commited by user2

---
+++ src/restapidemo/user2.java
@@ -0,0 +1,10 @@
+package restapidemo;
+
+public class user2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome its user2 ");
+	}
+
+}
